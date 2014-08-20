323f9440c8381c987edba22d763721194944465f
gaoz <gaoz@hawaii.edu>
2014-08-20 09:40:36 -1000

    fdasf

--- asset/js/parallax_slidershow_controller.js
+++ asset/js/parallax_slidershow_controller.js
@@ -9,40 +9,27 @@
  */
 
 ( function( $ ) {
-	// Init Skrollr
-	var s = skrollr.init();
-	 
-	// Refresh Skrollr after resizing our sections
-	s.refresh($('.homeSlide'));
-	
-	
-	// Init Skrollr
-	var s = skrollr.init({
-		forceHeight: false
-	});
 	
 	// Setup variables
 	$window = $(window);
 	$slide = $('.homeSlide');
 	$body = $('body');
 	
-    //FadeIn all sections   
-	$body.imagesLoaded( function() {
-		setTimeout(function() {
-		      
 		      // Resize sections
 		      adjustWindow();
 		      
-		      // Fade in sections
-			  $body.removeClass('loading').addClass('loaded');
-			  
-		}, 800);
-	});
 	
 	function adjustWindow(){
 		
 		// Init Skrollr
-		
+		var s = skrollr.init({
+		    forceHeight: false,
+		    render: function(data) {
+		    
+		        //Debugging - Log the current scroll position.
+		        //console.log(data.curTop);
+		    }
+		});
 		
 		// Get window size
 	    winH = $window.height();
@@ -56,7 +43,7 @@
 	    $slide.height(winH);
 	    
 	    // Refresh Skrollr after resizing our sections
-	    
+	    s.refresh($('.homeSlide'));
 	    
 	}
 		
