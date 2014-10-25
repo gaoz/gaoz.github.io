c9e9a831bd7f1645a6a77c045fb27aa0c94d2d5e
Vinson gao <vinsonggg@gmail.com>
2014-10-24 14:19:44 -1000

    sdf

--- asset/js/custom.js
+++ asset/js/custom.js
@@ -354,6 +354,7 @@ $('.delete-person').on("click", function () {
 });
 
 $('.delete-item').on("click", function () {
+	
     $(this).parents('div.item').fadeOut("slow", function() {
 		$(this).remove();
 		
@@ -374,5 +375,54 @@ setInterval(function(){
 }, 5000)
 */
 
+
+
+/*
+	Add to cart fly effect with jQuery. - May 05, 2013
+	(c) 2013 @ElmahdiMahmoud - fikra-masri.by
+	license: http://www.opensource.org/licenses/mit-license.php
+*/   
+
+
+$('.add').on('click', function () {
+		alert("test");
+        var cart = $('.glyphicon-shopping-cart');
+        var imgtodrag = $(this).parent('.add-to-cart').find("span").eq(0);
+        if (imgtodrag) {
+            var $img_to_move = $('<img src="http://cdn1.iconfinder.com/data/icons/jigsoar-icons/24/_cart.png" />');
+            var imgclone = $img_to_move
+                .offset({
+                top: imgtodrag.offset().top,
+                left: imgtodrag.offset().left
+            })
+                .css({
+                'opacity': '0.5',
+                    'position': 'absolute',
+                    'height': '150px',
+                    'width': '150px',
+                    'z-index': '100'
+            })
+                .appendTo($('body'))
+                .animate({
+                'top': cart.offset().top + 10,
+                    'left': cart.offset().left + 10,
+                    'width': 75,
+                    'height': 75
+            }, 1000, 'easeInOutExpo');
+            
+            setTimeout(function () {
+                cart.effect("shake", {
+                    times: 2
+                }, 200);
+            }, 1500);
+
+            imgclone.animate({
+                'width': 0,
+                    'height': 0
+            }, function () {
+                $(this).detach()
+            });
+        }
+    });
 
 
