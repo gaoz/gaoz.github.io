af045fc8dceb6a5a94214ba88b24f2f23d244a51
Vinson gao <vinsonggg@gmail.com>
2014-10-24 14:42:29 -1000

    fasfdsa

--- asset/js/custom.js
+++ asset/js/custom.js
@@ -384,45 +384,6 @@ setInterval(function(){
 */   
 
 
-$('.add').on('click', function () {
-		alert("test");
-        var cart = $('.glyphicon-shopping-cart');
-        var imgtodrag = $(this).parent('.add-to-cart').find("span").eq(0);
-        if (imgtodrag) {
-            var $img_to_move = $('<img src="http://cdn1.iconfinder.com/data/icons/jigsoar-icons/24/_cart.png" />');
-            var imgclone = $img_to_move
-                .offset({
-                top: imgtodrag.offset().top,
-                left: imgtodrag.offset().left
-            })
-                .css({
-                'opacity': '0.5',
-                    'position': 'absolute',
-                    'height': '150px',
-                    'width': '150px',
-                    'z-index': '100'
-            })
-                .appendTo($('body'))
-                .animate({
-                'top': cart.offset().top + 10,
-                    'left': cart.offset().left + 10,
-                    'width': 75,
-                    'height': 75
-            }, 1000, 'easeInOutExpo');
-            
-            setTimeout(function () {
-                cart.effect("shake", {
-                    times: 2
-                }, 200);
-            }, 1500);
 
-            imgclone.animate({
-                'width': 0,
-                    'height': 0
-            }, function () {
-                $(this).detach()
-            });
-        }
-    });
 
 
